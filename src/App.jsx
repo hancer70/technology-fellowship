5165870097bbcc8c08254bb3ac98a2b031d973d0
hancer70 <hancerm@hotmail.com>
2026-02-16 21:34:37 -0500

    Switch to HashRouter to fix 404 errors on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import WizardContainer from './components/wizard/WizardContainer';
 import ToolkitViewer from './pages/ToolkitViewer';
